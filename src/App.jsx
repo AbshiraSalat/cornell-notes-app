3116fec089520655f1a5c136f1c5ef30746a8592
abshirasalat <abshirasalat@gmail.com>
2025-11-21 02:13:35 -0800

    Fix SharedNote import once and for all

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { useStore } from './store/useStore';
-import SharedNote from './pages/SharedNote';
-
-
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -12,7 +9,6 @@ import Dashboard from './pages/Dashboard';
 import ClassNotes from './pages/ClassNotes';
 import NoteEditor from './pages/NoteEditor';
 import SharedNote from './pages/SharedNote';
-
 
 function App() {
   const theme = useStore(state => state.theme);
@@ -27,10 +23,9 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/class/:classId" element={<ClassNotes />} />
           <Route path="/note/:noteId" element={<NoteEditor />} />
-          <Route path="*" element={<Navigate to="/login" />} />
           <Route path="/share/:noteId" element={<SharedNote />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
-
         <Toaster position="top-right" />
       </Router>
     </div>
